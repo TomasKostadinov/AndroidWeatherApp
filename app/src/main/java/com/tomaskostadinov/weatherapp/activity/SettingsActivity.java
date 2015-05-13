e2add64ed6ac91a9197b93ddd242bf498cb76561
Tomas Kostadinov <tomas.kostadinov@gmx.de>
2015-05-13 13:10:19 +0200

    Bug fixes

---
+++ app/src/main/java/com/tomaskostadinov/weatherapp/activity/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.tomaskostadinov.weatherapp.activity;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+import com.tomaskostadinov.weatherapp.R;
+
+/**
+ * Created by Tomas on 13.05.2015.
+ */
+public class SettingsActivity extends PreferenceActivity {
+
+
+}
